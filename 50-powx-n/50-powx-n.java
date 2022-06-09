0f7b89c232653820d5ff428eb59081368915d5f0
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2022-06-09 23:38:21 +0530

    Time: 2 ms (15.34%), Space: 41.9 MB (70.27%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,29 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0){
+                     return 1;
+                       }
+					 else if( n > 0)
+					 {
+					  if(n%2 == 0)
+					  {
+					  return myPow(x*x , n/2);
+					  }
+					  else
+					  {
+					  return myPow(x*x, (n-1)/2) * x;
+					  }
+					  }
+			      else
+			      {
+			        if(n%2 == 0)
+					 { 
+					  return myPow(x*x , n/2);
+					  }
+					  else
+					  {
+					  return myPow(x*x, (n+1)/2)* (1/x) ;
+					  }
+					 }
+    }
+}
