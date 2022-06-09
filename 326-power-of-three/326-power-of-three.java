55301dc9a76aa9c8805a82b228b6aa4fcce7041c
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2022-06-09 22:56:45 +0530

    Time: 31 ms (10.34%), Space: 47 MB (58.84%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+       if(n<=0)
+           return false;
+        if(n%3==0)
+            return isPowerOfThree(n/3);
+        if(n==1)
+            return true;
+        else 
+            return false;
+    }
+}
