8b0164b9f5c03d731424493de190eb3d68adfdea
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2022-08-30 08:44:26 +0530

    Time: 2 ms (100.00%), Space: 41.7 MB (78.90%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,16 +1,12 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length][amount + 1];
-        for(var i: dp) Arrays.fill(i,-1);
-       return count(amount,coins,coins.length - 1,dp); 
-    }
-    private int count(int amount, int[] coins, int ind,int[][] dp) {
-        if(amount == 0) return 1;
-        if(ind < 0) return 0;
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        int NotTake = count(amount,coins,ind-1,dp);
-        int take = 0;
-        if(amount >= coins[ind]) take = count(amount - coins[ind],coins, ind,dp);
-        return dp[ind][amount] = take + NotTake;
+        int[] ch = new int[amount+1];
+        ch[0]=1;
+        for(int coin:coins){
+            for(int i=coin;i<amount+1;i++){
+                ch[i]+=ch[i-coin];
+            }
+        }
+        return ch[amount];
     }
 }
