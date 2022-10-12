194c5c38bc8c249ab9c400e7acf0143e2e1ebf5c
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2022-10-12 22:36:10 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (91.81%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,15 +14,21 @@
  * }
  */
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-            List<Integer>list=new ArrayList<Integer>();
-            dfs(root,list);
-            return list;
-    }
-    private void dfs(TreeNode node,List<Integer>list){
-        if(node==null) return;
-        list.add(node.val);
-        dfs(node.left,list);
-        dfs(node.right,list);
+    public List<Integer> preorderTraversal(TreeNode node) {
+        List<Integer> list=new LinkedList<Integer>();
+        Stack<TreeNode> right = new Stack<TreeNode>();
+        while(node !=null){
+            list.add(node.val);
+            if(node.right!=null){
+                right.push(node.right);
+            }
+               if(node.left==null && !right.isEmpty()){
+                   node=right.pop();
+               }
+               else{
+                   node=node.left;
+               }
+        }
+        return list;
     }
 }
