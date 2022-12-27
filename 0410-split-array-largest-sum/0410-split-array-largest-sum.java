a6da638f720b425f82ba4b82095fb6e4162c8dd0
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2022-12-27 21:30:16 +0530

    Time: 1 ms (85.53%), Space: 40.1 MB (79.81%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,16 +1,20 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
+        //first we will find the min and max ans that we can get from the given arrray our ans must lie betweem this range only
         int h = 0;
         int l = 0;
         for(int i=0;i<nums.length;i++){
             l = Math.max(l, nums[i]);
             h = h+ nums[i];
         }
+        //now declear the answer as the lowest possible value
         int ans=l;
+        //apply simple binary search
         while(h>=l){
             int mid=(h+l)/2;
             int sum=0;
             int count=1;
+            // the loop will give us the maximum number of the sub array that can be given which has the sum equal to the mid value
             for(int i=0;i<nums.length;i++){
                 if(sum+nums[i]>mid){
                     sum=nums[i];
@@ -19,6 +23,7 @@ class Solution {
                 else
                     sum=sum+nums[i];
             }
+            //now if the no. of sub array in the which has sum equal to mid are greater the k then we change our low value other wise we change the high and ans euual to that mid value
             if(count>k){
                 l=mid+1;
             }
