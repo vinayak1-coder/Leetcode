0c90f55146db67995c3570e404c62954e9c4718e
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2023-01-31 22:37:58 +0530

    Time: 48 ms (65.99%), Space: 42.8 MB (51.70%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.java
@@ -0,0 +1,38 @@
+class Player{
+        
+        public int age;
+        public int score;
+        
+        public Player(int age, int score){
+            this.age = age;
+            this.score = score;
+        }
+    
+    }
+class Solution {
+    public int bestTeamScore(int[] scores, int[] ages) {
+       Player[] players = new Player[scores.length];
+        for(int i=0;i<players.length;i++){
+            players[i] = new Player(ages[i],scores[i]);
+        }
+        Arrays.sort(players, (player1,player2)->player1.age==player2.age?player1.score-player2.score:player1.age-player2.age);
+        
+        int dp[] = new int[players.length];
+        int max = dp[0] = players[0].score;
+        for(int i=1;i<players.length;i++){
+            dp[i] = players[i].score;
+            for(int j=0;j<i;j++){
+                if(players[j].score<=players[i].score){
+                    dp[i] = Math.max(dp[i],dp[j]+players[i].score);   
+                }   
+            }      
+            max = Math.max(max,dp[i]);
+            
+        }
+        
+        
+        
+        return max;
+      
+    }
+}
