95f2c797e14cede8e75dbe381420ff15d6bb6d67
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2023-03-18 10:46:13 +0530

    Time: 1 ms (55.31%), Space: 43.3 MB (14.72%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,21 +15,30 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        List<Integer> list=new ArrayList<>();
-        Stack<TreeNode> stack=new Stack<>();
-        TreeNode curr=root;
-        while(curr != null || !stack.isEmpty()){
-            while(curr != null){
-                stack.push(curr);
-                curr=curr.left;
-            }
-            curr=stack.pop();
-            list.add(curr.val);
-            curr=curr.right;
-        }
+//         List<Integer> list=new ArrayList<>();
+//         Stack<TreeNode> stack=new Stack<>();
+//         TreeNode curr=root;
+//         while(curr != null || !stack.isEmpty()){
+//             while(curr != null){
+//                 stack.push(curr);
+//                 curr=curr.left;
+//             }
+//             curr=stack.pop();
+//             list.add(curr.val);
+//             curr=curr.right;
+//         }
         
-            int temp=list.get(k-1);
+//             int temp=list.get(k-1);
         
-        return temp;
+//         return temp;
+        List<Integer> arr = new ArrayList<>();
+        inOrder(root, arr);
+        return arr.get(k - 1);
+    }
+    private void inOrder(TreeNode root,List<Integer> arr) {
+        if(root == null) return;
+        inOrder(root.left, arr);
+        arr.add(root.val);
+        inOrder(root.right, arr);
     }
 }
