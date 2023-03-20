00097755bebe5fce5b5aa17ff7950f8926ff660b
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2023-03-20 12:05:57 +0530

    Time: 2 ms (98.38%), Space: 42.3 MB (48.55%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,7 +15,9 @@
  */
 class Solution {
    //private int preStart = 0;
+    Map<Integer,Integer> map = new HashMap<>();
 public TreeNode buildTree(int[] preorder, int[] inorder) {
+    for(int i = 0; i < inorder.length; i++) map.put(inorder[i], i);
        return newTree(preorder,inorder,0,preorder.length-1,0,inorder.length-1);
     }
     private TreeNode newTree(int[] preorder, int[] inorder, int pstart,int pend, int istart,int iend){
@@ -23,13 +25,7 @@ public TreeNode buildTree(int[] preorder, int[] inorder) {
             return new TreeNode(preorder[pstart]);
         }
         if(pstart > pend) return null;
-        int idx = 0;
-        for(int i = 0; i < inorder.length; i++) {
-            if(inorder[i] == preorder[pstart]) {
-                idx = i;
-                break;
-            }
-        }
+        int idx = map.get(preorder[pstart]);
         TreeNode root = new TreeNode(inorder[idx]);
         root.left=newTree(preorder,inorder,pstart + 1, pstart + idx - istart,istart, idx - 1);
         root.right=newTree(preorder,inorder,pstart + idx - istart + 1,pend, idx + 1, iend);
