6ad2e0a548dffa479548f9d5dc736e05c48aac21
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2023-03-27 08:54:12 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (22.70%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,5 +1,12 @@
 class Solution {
     public int getSum(int a, int b) {
-        return (a+b);
+        int x=a^b;
+        int c=a&b;
+        if(c==0){
+            return x;
+        }
+        else{
+            return getSum(x,c<<1);
+        }
     }
 }
