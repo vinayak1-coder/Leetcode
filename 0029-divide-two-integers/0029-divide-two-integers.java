cbcba6b12e32d225d66215b20136acfb6753dc8c
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2023-04-03 08:32:44 +0530

    Time: 1 ms (100.00%), Space: 39.7 MB (72.33%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,9 +1,12 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        int s=dividend/divisor;
-        if(dividend==-2147483648 && divisor==-1){
-            return 2147483647;
-        }
-        return s;
+        if (dividend == 1 << 31 && divisor == -1) return (1 << 31) - 1;
+        int a = Math.abs(dividend), b = Math.abs(divisor), res = 0;
+        for (int x = 31; x >= 0; x--)
+            if ((a >>> x) - b >= 0) {
+                res += 1 << x;
+                a -= b << x;
+            }
+        return (dividend > 0) == (divisor > 0) ? res : -res;
     }
 }
