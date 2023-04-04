ac4d88ba243d6aee3f312f90541eb4a27c9bdced
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2023-04-04 10:36:53 +0530

    Time: 1 ms (95.22%), Space: 42.9 MB (37.73%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        per(nums,ans,list,new boolean[nums.length]);
+        return ans;
+    }
+    private void per(int[] nums,List<List<Integer>> ans,List<Integer> list,boolean[] visited){
+        if (list.size() == nums.length) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+         for (int i = 0; i < nums.length; i++) {
+            if (visited[i])
+                continue;
+            list.add(nums[i]);
+            visited[i] = true;
+            per(nums,ans,list,visited);
+            visited[i] = false;
+            list.remove(list.size() - 1);
+        }
+    }
+}
