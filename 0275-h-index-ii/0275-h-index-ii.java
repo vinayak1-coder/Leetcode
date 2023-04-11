2db1a4445c642edc2820a2a47791e7fd3fa0d621
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2023-04-11 08:38:41 +0530

    Time: 0 ms (100.00%), Space: 49.9 MB (30.47%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,13 +1,20 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n=citations.length;
-        int c=0;
-        for(int i=0;i<citations.length;i++){
-            if((n-i)<=citations[i]){
-                c=n-i;
-                break;
+        int high=citations.length-1;
+        int low=0;
+        int ans=0;
+        while(high>=low){
+            int mid=low+(high-low)/2;
+            if((n-mid)<=citations[mid]){
+                ans=Math.max(n-mid,ans);
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-        return c;
+        return ans;
+        
     }
 }
