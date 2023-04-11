ebda52a191a25cb06853093ab8868d8ef78550fc
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2023-04-11 08:40:45 +0530

    Time: 8 ms (30.61%), Space: 39.6 MB (88.33%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,9 +1,9 @@
 class Solution {
     public int arrangeCoins(int n) {
         int count=1;
-        // if(n==1){
-        //     return 1;
-        // }
+        if(n==1){
+            return 1;
+        }
         while(n>0){
             count++;
             n=n-count;
