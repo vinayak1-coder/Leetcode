74a39a6da0594b470a4659340bb3b3079d626076
Vinayak Pathak <82221546+vinayak1-coder@users.noreply.github.com>
2023-06-01 22:54:26 +0530

    Time: 15 ms (79.20%), Space: 45.2 MB (12.16%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,42 +1,27 @@
 class Solution {
-   private int dir[][] = new int[][]{{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
-
     public int shortestPathBinaryMatrix(int[][] grid) {
-
-        int m = grid.length;
-        int n = grid[0].length;
-
-        if(grid[0][0]==1 || grid[m-1][n-1]==1) {
-            return -1;
+        if(grid[0][0] == 1) return -1;
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] {0,0,0});
+        int[] dx = {-1,1,0,0,-1,1,-1,1};
+        int[] dy = {0,0,1,-1,-1,1,1,-1};
+        while(!queue.isEmpty()) {
+            int a = queue.peek()[0];
+            int b = queue.peek()[1];
+            int dist = queue.peek()[2];
+            queue.poll();
+            if(a == grid.length - 1 && b == grid.length - 1) {
+                return dist + 1; 
+            } 
+           
+            for(int i = 0; i < 8; i++) {
+                int x = a + dx[i];
+                int y = b + dy[i];
+                if(x < 0 || y < 0 || x >= grid.length || y >= grid.length || grid[x][y] != 0) continue;
+                grid[x][y] = -1;
+                queue.add(new int[]{x,y,dist + 1});
+            } 
         }
-
-        boolean[][] visited = new boolean[m][n];
-        visited[0][0] = true;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0});
-
-        int ans=0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for(int i=0;i<size;i++) {
-                int[] pop = queue.remove();
-                if(pop[0]==m-1 && pop[1]==n-1) {
-                    return ans+1;
-                }
-                for (int k=0;k<8;k++) {
-                    int nextX = dir[k][0]+pop[0];
-                    int nextY = dir[k][1]+pop[1];
-
-                    if(nextX>=0 && nextX<m && nextY>=0 && nextY<n && !visited[nextX][nextY] && grid[nextX][nextY]==0) {
-                        queue.add(new int[]{nextX,nextY});
-                        visited[nextX][nextY]=true;
-                    }
-
-                }
-            }
-            ans++;
-        }
-
         return -1;
     }
 }
